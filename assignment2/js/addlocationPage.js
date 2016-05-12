00dcf973a8a1228899f0e72d5aa21397d088ef27
cvon10 <cvon10@student.monash.edu>
2016-05-12 11:44:15 +1000

    Add Assignment 2
    Add Assignment 2

---
+++ assignment2/js/addlocationPage.js
@@ -0,0 +1,12 @@
+// Code for the Add Location page.
+function addLocation () {
+console.log("done");
+}
+        function initMap() 
+        {
+            var mapDiv = document.getElementById('map"');
+            var map = new google.maps.Map(mapDiv, {
+                centre: {lat: 44.540, lng: -78.546},
+                zoom: 8
+            });
+        };
