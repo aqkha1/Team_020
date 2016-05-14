5d154ad743b0a3f949f308293cb4d9f72698271a
cvon10 <cvon10@student.monash.edu>
2016-05-14 14:30:52 +1000

    Worked on Add location
    -Map
    -Input
    -Marker

--- assignment2/js/addlocationPage.js
+++ assignment2/js/addlocationPage.js
@@ -1,12 +1,39 @@
 // Code for the Add Location page.
-function addLocation () {
-console.log("done");
-}
-        function initMap() 
-        {
-            var mapDiv = document.getElementById('map"');
-            var map = new google.maps.Map(mapDiv, {
-                centre: {lat: 44.540, lng: -78.546},
-                zoom: 8
+var map;
+var infowindow;
+
+function initMap () 
+{
+    var geocoder = new google.maps.Geocoder ();
+    var mapDiv = document.getElementById('map');
+    map = new google.maps.Map(mapDiv, 
+                {
+                center: {lat: -37.9120467, lng: 145.1343136},
+                zoom: 13
+                });
+                document.getElementById("address").addEventListener('keyup', function() {
+                geocodeAddress(geocoder, map);
+                });
+                document.getElementById('btn1').addEventListener('click', function () { addLocation();
+                });
+};
+
+function geocodeAddress(geocoder) {
+        var address = document.getElementById("address").value;
+             geocoder.geocode( { 'address': address}, function(results, status) {
+                 map.setCenter(results[0].geometry.location); 
+                 var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location,
+                    title : results[0].formatted_address
+                });
+                 infowindow = new google.maps.InfoWindow({
+                                content: results[0].formatted_address
+                 });
+                 infowindow.open(map, marker);
             });
-        };
+};
+
+function addLocation () {
+   locations.push() 
+};
